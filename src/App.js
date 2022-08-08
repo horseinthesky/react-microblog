c572e4508ca2a6cdd35368bbeeeb657948e7ccac
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-08-08 20:14:41 +0300

    chapter 8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Container from "react-bootstrap/Container";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
+
 import ApiProvider from "./contexts/ApiProvider";
 import FlashProvider from "./contexts/FlashProvider";
-import Header from "./components/Header";
+import UserProvider from "./contexts/UserProvider";
+
 import FeedPage from "./pages/FeedPage";
 import ExplorePage from "./pages/ExplorePage";
 import UserPage from "./pages/UserPage";
@@ -15,15 +21,40 @@ export default function App() {
       <BrowserRouter>
         <FlashProvider>
           <ApiProvider>
-            <Header />
-            <Routes>
-              <Route path="/" element={<FeedPage />} />
-              <Route path="/explore" element={<ExplorePage />} />
-              <Route path="/user/:username" element={<UserPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegistrationPage />} />
-              <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
+            <UserProvider>
+              <Header />
+              <Routes>
+                <Route
+                  path="/login"
+                  element={
+                    <PublicRoute>
+                      <LoginPage />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="/register"
+                  element={
+                    <PublicRoute>
+                      <RegistrationPage />
+                    </PublicRoute>
+                  }
+                />
+                <Route
+                  path="*"
+                  element={
+                    <PrivateRoute>
+                      <Routes>
+                        <Route path="/" element={<FeedPage />} />
+                        <Route path="/explore" element={<ExplorePage />} />
+                        <Route path="/user/:username" element={<UserPage />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                      </Routes>
+                    </PrivateRoute>
+                  }
+                />
+              </Routes>
+            </UserProvider>
           </ApiProvider>
         </FlashProvider>
       </BrowserRouter>
